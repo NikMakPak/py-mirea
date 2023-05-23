bb4bfc89d2b4d055f0f0249ad5996e7fc7a8c4fa
nikmakpak <nicement28@gmail.com>
2023-05-23 12:39:40 +0300

    feat: add app structure

---
+++ main.py
@@ -0,0 +1,27 @@
+'''
+Архитектура приложения:
+- Main
+- Child
+- Update
+- Search
+- SearchBetween
+- DB
+'''
+
+class Main():
+     def __init__(self):
+    
+class Child():
+     def __init__(self):   
+
+class Update(Child):
+     def __init__(self):
+          
+class Search():
+     def __init__(self):
+
+class SearchBetween():
+     def __init__(self):
+
+class DB:
+     def __init__(self):
