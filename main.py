f229b82eb2da833fbd8245c59ec29925a0d95943
Danil <galnykin.03@mail.ru>
2023-05-26 15:01:17 +0300

    feat: add init DB and Main

--- main.py
+++ main.py
@@ -7,9 +7,25 @@
 - SearchBetween
 - DB
 '''
+import tkinter as tk
+import sqlite3
+import pytesseract
+from tkinter import *
+from tkinter import ttk, filedialog
+from PIL import Image, ImageTk
+import cv2
+import time
 
-class Main():
-     def __init__(self):
+pytesseract.pytesseract.tesseract_cmd = \
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+class Main(tk.Frame):
+	def __init__(self, root):
+        super().__init__(root)
+        self.init_main()
+        self.db = db
+        self.view_records()
+
     
 class Child():
      def __init__(self):   
@@ -24,4 +40,25 @@ class SearchBetween():
      def __init__(self):
 
 class DB:
-     def __init__(self):
+    def __init__(self):
+        self.conn = sqlite3.connect('DataBase.db')
+        self.c = self.conn.cursor()
+        self.c.execute(
+            '''CREATE TABLE IF NOT EXISTS DataBase (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , 
+            lastName text, Name text, middleName text, both text, city text, 
+            serialNumber integer, dateReg text, placeIssue text, 
+            divisionCode integer, agresReg text, SNILS text, tax text, 
+            birthCertificat text, issueCertificate text, LNMFather text,
+            LNMMather text, LinkPhoto text )''')
+        self.conn.commit()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    db = DB()
+    app = Main(root)
+    app.pack()
+    root.title("ReadPass")
+    root.geometry("1280x720")
+    root.resizable(True, True)
+    root.mainloop()
